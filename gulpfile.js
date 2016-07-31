3d5610b16b7322e90e70b943a644cd2f26c39a05
Chen Yang <yangc5@mail.uc.edu>
2016-07-31 13:53:45 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var usemin = require('gulp-usemin');
+var minifyHtml = require('gulp-minify-html');
+var rev = require('gulp-rev');
+
+gulp.task('clean', function (cb) {
+    del([
+        // everything inside the dist folder
+        'dist/**/*'
+    ], cb);
+});
+
+gulp.task('usemin', function () {
+    return gulp.src('./*.html')
+        .pipe(usemin({
+            html: [minifyHtml({empty: true, conditionals:true})],
+            js: [uglify(), 'concat', rev()]
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', ['clean', 'usemin']);
