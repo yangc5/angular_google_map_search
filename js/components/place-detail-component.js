bbb03f1131241650bffaeb63a79a9b941426bc38
Chen Yang <yangc5@mail.uc.edu>
2016-08-01 14:15:36 -0500

    angular components stubbing

---
+++ js/components/place-detail-component.js
@@ -0,0 +1,19 @@
+var Place = {
+  controller: function($scope) {
+    this.place = {id: 1, name: 'Sydney', description: 'capital of Australia lorem'};
+  },
+  controllerAs: 'placeCtrl',
+  template: [
+    '<div class="thumbnail">',
+    '<img src="http://www.australia.com/content/australia/en/places/sydney/nsw-sydney-harbour/_jcr_content/image.adapt.761.medium.jpg" alt="sydney">',
+    '<div class="caption">',
+    '<h3>{{placeCtrl.place.name}}</h3>',
+    '<p><a href="#" class="btn btn-primary" role="button">Website</a></p>',
+    '<p>{{placeCtrl.place.description}}</p>',
+    '</div>',
+    '</div>'
+  ].join('')
+}
+
+angular.module('app')
+       .component('place', Place);
---
+++ js/components/places-component.js
@@ -0,0 +1,23 @@
+var Places = {
+  controller: function($scope) {
+    this.places = [
+      {id: 1, name: 'Sydney', description: 'capital of Australia'},
+      {id: 2, name: 'Austin', description: 'Texas capital'},
+      {id: 3, name: 'Shanghai', description: 'largest city in China'}
+    ];
+  },
+  controllerAs: 'placesCtrl',
+  template: [
+    '<div ng-repeat="place in placesCtrl.places" class="thumbnail">',
+    '<img src="http://www.australia.com/content/australia/en/places/sydney/nsw-sydney-harbour/_jcr_content/image.adapt.761.medium.jpg" alt="sydney">',
+    '<div class="caption">',
+    '<h3>{{place.name}}</h3>',
+    '<p>{{place.description}}</p>',
+    '<p><a href="#" class="btn btn-primary" role="button">Button</a></p>',
+    '</div>',
+    '</div>'
+  ].join('')
+}
+
+angular.module('app')
+       .component('places', Places);
